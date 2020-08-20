25e5f6f8d2263eebd2f85df42814365766e69306
yhy729 <hongyinglove@126.com>
2020-08-20 16:40:08 +0800

    添加项目文件。

---
+++ AlgorithmApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Algorithm.App
+{
+    /// <summary>
+    /// 本项目主要用于记录基础算法的研究与学习
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************客户端程序调用开始****************************");
+            try
+            {
+                Console.WriteLine("Hello World!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"程序出错:{ex.Message}");
+            }
+            Console.WriteLine("***************************客户端程序调用完成****************************");
+            Console.ReadKey();
+        }
+    }
+}
